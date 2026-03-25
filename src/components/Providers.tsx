8644adf1be170d1c218b4849c7af91b073e9bfcf
ronitrajfr <ronitrajofficial7@gmail.com>
2026-03-25 17:36:44 +0530

    feat: added next theme

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,9 +3,21 @@
 import { ConvexAuthProvider } from "@convex-dev/auth/react";
 import { ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
+import { ThemeProvider } from "./ui/theme-provider";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
-export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  return <ConvexAuthProvider client={convex}>{children}</ConvexAuthProvider>;
+export function Providers({ children }: { children: ReactNode }) {
+  return (
+    <ConvexAuthProvider client={convex}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </ConvexAuthProvider>
+  );
 }
